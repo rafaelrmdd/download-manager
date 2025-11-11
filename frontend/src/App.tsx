77196c201ab633d619d66fc5b6044da52fb17589
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-11-11 16:00:32 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="w-[400px]">
+        <header className="flex justify-between items-center">
+            <div className="flex gap-x-5">
+                Icon
+                <h1>Download Manager</h1>
+            </div>
+
+            <span>1 Active</span>
+        </header>
+    </div>
+  )
+}
+
+export default App
