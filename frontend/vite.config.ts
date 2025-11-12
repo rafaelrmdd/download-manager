dd5fd35128cf0f235277ee9382f1cd20c53a056a
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-11-12 14:38:39 -0300

    add chrome service worker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/service-worker.ts',  // path in your project
+          dest: '.'                      // copy to dist root
+        }
+      ]
+    }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        serviceWorker: 'src/service-worker.ts',
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  }
 })
